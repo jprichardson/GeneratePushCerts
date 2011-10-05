0b2d190ff800744c8cdf5875c2869f34eafacb8a
JP Richardson <jprichardson@gmail.com>
2011-10-04 22:05:09 -0500

    At point where it needs to configure the certificate.

--- main.rb
+++ main.rb
@@ -6,32 +6,59 @@ config = YAML::load(File.open(Dir.pwd + '/config.yml'))
 USER = config['user']
 PASSWORD = config['password']
 
-browser = Watir::Browser.new(:chrome)
+def main
+  puts("Let's get this party started.'")
+  browser = Watir::Browser.new(:chrome)
 
-browser.goto("https://developer.apple.com/ios/manage/bundles/index.action")
-table = nil
+  browser.goto("https://developer.apple.com/ios/manage/bundles/index.action")
+  table = nil
 
-if browser.body.text.include?('Sign in')
-  puts("Not logged in... logging in...")
-  browser.text_field(name: 'theAccountName').set(USER)
-  browser.text_field(id: 'accountpassword').set(PASSWORD)
-  form = browser.form(name:'appleConnectForm')
-  form.submit()
-  puts("we're in!'")
+  if browser.body.text.include?('Sign in')
+    puts("Not logged in... logging in...")
+    browser.text_field(name: 'theAccountName').set(USER)
+    browser.text_field(id: 'accountpassword').set(PASSWORD)
+    form = browser.form(name:'appleConnectForm')
+    form.submit()
+    puts("we're in!")
+  end
+
+  table  = browser.div(:class => 'nt_multi').table #table of Apple App Ids, now called AAID
+
+  count = table.rows.count
+  0.upto(count - 1) do |i|
+    tds = table[i]
+    if tds[0].strong.exists?
+      unless tds[0].strong.attribute_value(:title).nil?
+        aaid = tds[0].strong.attribute_value(:title).strip
+        if aaid.ends_with?('FanFB')
+          if tds[1].text.include?('Enabled for Production')
+            puts "#{aaid} already enabled. Skipping..."
+          elsif tds[1].text.include?('Configurable for Production')
+            puts "Configuring certificate for #{aaid}..."
+            
+          end
+        end
+      end
+    end
+
+  end
+
+  puts('Done. Press [ENTER] to exit.')
+  STDIN.gets.strip
+
 end
 
-table  = browser.div(:class => 'nt_multi').table #table of Apple App Ids, now called AAID
-
-#puts table.html
-
-count = table.rows.count
-0.upto(count - 1) do |i|
-  td = table[i][0]
-  puts td
-  #puts table[i][0].strong.html
+def configure_cert(browser)
+  
 end
 
+class String
 
+  def ends_with?(suffix)
+    suffix = suffix.to_s
+    self[-suffix.length..-1] == suffix
+  end
 
-puts('Done. Press [ENTER] to exit.')
-STDIN.gets.strip
+end
+
+main()
